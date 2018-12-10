087fa22d5c45bc0fd41d04945100e8bb51f0c37a
Elle <ellexidecodes@gmail.com>
2018-12-11 06:43:29 +0800

    fix name clipping and change placeholder image

--- red/lobelia.js
+++ red/lobelia.js
@@ -1,7 +1,7 @@
 $(document).ready(()=>{$(".box").prepend("<div class=\"blurBack\"></div> <div class=\"lobelia\"></div>");var allFriends=$("a.friend").length;var animationDuration=allFriends*5;console.log(allFriends);$("#marquee").css("animation-duration",`${animationDuration}s`);$('.friend').each(function(){var image=$(this).attr("image");var name=$(this).attr("name");var desc=$(this).attr("desc");$(this).addClass("tooltip");if(!name&&!desc){$(this).attr("title","Coming Soon!");}else{if(!name){name="";}else{name=`<h1>${name}</h1>`;}
 if(!desc){desc="";}else{desc=`<p>${desc}</p>`;}
 $(this).attr("title",`${name}${desc}`);}
-if(!image){image="http://www.ieeeaustsb.org/files/2017/05/placeholder-female-square.png";}
+if(!image){image="https://i.imgur.com/ac7ICFp.png";}
 $(this).attr("style",`background-image:url(${image})`);});$(".nav div").each(function(){$(this).removeClass("lotus");$(this).html("<div class='lotus'></div>");})
 $(".nav>div").click(function(){var allNav=$(".nav>div");var open=$(this).attr("opens");allNav.removeClass("selected");$(this).addClass("selected");$(".tab").hide();$(`#${open}`).show();})
 $(".flexBox .image").each(function(){var background=$(this).attr("image");$(this).css("background-image",`url(${background})`);})
