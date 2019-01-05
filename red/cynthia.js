cb18e96212d51e5735ece88360a0e63a5a8f9044
Ellexide <ellexidecodes@gmail.com>
2019-01-05 11:58:38 +0800

    REMOVE CUSTOM SCROLLBAR

--- red/cynthia.js
+++ red/cynthia.js
@@ -1,4 +1,3 @@
-$(document).load(function(){$(".tab").mCustomScrollbar({theme:"rounded-dots"});});
 $(document).ready(()=>{$(".eye").click(function(){var opens=$(this).attr("opens");if($(opens).is(":visible")){return;}
 $(".eye").removeClass("selected");$(this).addClass("selected");$(".tab").fadeOut(500);$(opens).fadeIn(500);})
 $(".tab").hover(function(){$(this).addClass("hovered");},function(){$(this).removeClass("hovered")});$(".friend .face").each(function(){$(this).css("background-image",`url(${$(this).attr("img")})`);})})
